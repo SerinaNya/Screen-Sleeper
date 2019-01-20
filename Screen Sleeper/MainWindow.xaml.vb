e28ad4c8ab8d6efe87c521a5d44293098aa01e42
Jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2019-01-20 14:44:21 +0800

    Upload 0.0.0.0

---
+++ Screen Sleeper/MainWindow.xaml.vb
@@ -0,0 +1,13 @@
+﻿Class MainWindow
+    REM Written by:小金小Jin
+    REM .NET Framework 4.0
+
+    Public Sub Me_MouseRightButtonDown() Handles Me.MouseRightButtonDown
+        End
+    End Sub
+    Private Sub MainWindow_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.Key = Key.Escape Then
+            End
+        End If
+    End Sub
+End Class
